49ecefe4a7ad669996104a8ecddf66e7204d0d58
srinitin20 <srivastava.nitin2@gmail.com>
2025-06-13 07:30:04 +0530

    Added Usha

--- class/SQLite/main.py
+++ class/SQLite/main.py
@@ -3,5 +3,5 @@ import sqlite3
 from sqlExample import SQLExample
 
 connection = sqlite3.connect("EmployeeDB.db")
-emp = SQLExample('Bharat',678,178000)
+emp = SQLExample('Usha',127,178000)
 #emp.getEmployees(connection)
